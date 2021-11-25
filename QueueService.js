9a1985b2da4e00e6c3464919c261d61bcf272f00
Michał <michal@MacBook-Air-Micha.local>
2021-11-25 21:12:35 +0100

    added graceful exit

--- QueueService.js
+++ QueueService.js
@@ -2,34 +2,60 @@ const Queue = require('bull');
 
 class QueueService {
     constructor(queueName = 'default') {
-        this.queue = new Queue(queueName)
+        this.queue = new Queue(queueName, {
+        })
+
+        process.on('SIGTERM', async() => {
+            await this.queue.close();
+            process.kill(0);
+          });
+
+        process.on('SIGINT', async() => {
+            await this.queue.close();
+            console.info('\nGoodbye 👋');
+            process.kill(0);
+          });
     }
 
     async start(cron, jobName = 'default', data = {}) {
         await this.deleteOldJob(jobName);
         this.queue.process(jobName, async (job, done) => {
+            try {
+            await this.queue.pause(false, true);
+            const jobs = await this.queue.getActive();
+            // if(jobs.length > 1) {
+                // const t = await job.retry();
+                // console.log(t);
+            // }
+
             job.progress(10);
             console.log('Starting job: ' + job.id);
-            console.log(job.data);
+            console.log(jobs.length);
             setTimeout(() => {
                 job.progress(100);
                 console.log('Finising job: ' + job.id);
                 done();
+                this.queue.resume(false);
             }, 3000)
+        } catch(e) {
+            console.log('Error: ' + e);
+            done(e);
+        }
         });
 
-        this.addCronJob(cron, jobName, data);
+        setInterval(() => {
+            this.addCronJob(cron, jobName, data);
+        },10000)
 
         console.log('listening...')
     }
 
     async deleteOldJob(name) {
         const jobs = await this.queue.getRepeatableJobs();
-        console.log(jobs);
-
         // jobs.forEach(job => this.queue.removeRepeatableByKey(job.key)) // delete all jobs
 
         const job = jobs.find(job => job.name === name);
+        console.log(job);
         if(job) {
             console.log('Deleting old jobs...');
             await this.queue.removeRepeatableByKey(job.key);
@@ -38,9 +64,9 @@ class QueueService {
 
     addCronJob(cron= "* * * * *", name, data) {
         this.queue.add(name, data, {
-            repeat: {
-                cron,
-            }
+            // repeat: {
+                // cron,
+            // },
         })
     }
 
